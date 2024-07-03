bc82e3c723959c80382242e0015e271e48207bb0
AbdMajidKHn <majidkhan@gmail.com>
2024-07-03 12:53:17 +0500

    "Added media query to adjust margin top of 'Live Demo' button on smaller screens"

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -69,34 +69,42 @@ const Portfolio = () => {
 
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
-
-      <div className="container portfolio__container">
-        {soloProjects.map((pro) => (
-          <article className="portfolio__item" key={pro.id}>
-            <div className="portfolio__item-image">
-              <img src={pro.img} alt={pro.title} />
-            </div>
-            <div className="portfolio__item-content">
-              <h3>{pro.title}</h3>
-              <p>{pro.description}</p>
-              <p>{pro.technologies}</p>
-            </div>
-            <div className="portfolio__item-cta">
-              <a
-                href={pro.link}
-                target="_blank"
-                className="btn btn-primary"
-                rel="noreferrer"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
-        ))}
-      </div>
-    </section>
+    <h5>My Recent Work</h5>
+    <h2>Portfolio</h2>
+  
+    <div className="container portfolio__container">
+      {soloProjects.map((pro) => (
+        <article className="portfolio__item" key={pro.id}>
+          <div className="portfolio__item-image">
+            <img src={pro.img} alt={pro.title} />
+          </div>
+          <div className="portfolio__item-content">
+            <h3>{pro.title}</h3>
+            <p>{pro.description}</p>
+            <p>{pro.technologies}</p>
+          </div>
+          <div className="portfolio__item-cta">
+            <a
+              href={pro.link}
+              target="_blank"
+              className="btn btn-primary"
+              rel="noreferrer"
+              style={{
+                marginTop: '1rem', // Default margin top
+                // Media query for smaller screens
+                '@media screen and (max-width: 768px)': {
+                  marginTop: '0.5rem', // Adjusted margin top for smaller screens
+                }
+              }}
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      ))}
+    </div>
+  </section>
+  
   );
 };
 
